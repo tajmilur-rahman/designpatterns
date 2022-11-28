69989ca5b38ea95b9f5d127eee390cfbd133303b
ramasubbaiah333 <117220456+ramasubbaiah333@users.noreply.github.com>
2022-11-28 17:57:00 -0600

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -2,9 +2,9 @@ public class Driver {
    public static void main(String[] args) {
       AudioPlayer ap = new AudioPlayer();
 
-      ap.playmusic("mp3", "unstoppable.mp3");
-      ap.playmusic("mp4", "cradles.mp4");
-      ap.playmusic("vlc", "the box.vlc");
+      ap.playMusic("mp3", "unstoppable.mp3");
+      ap.playMusic("mp4", "cradles.mp4");
+      ap.playMusic("vlc", "the box.vlc");
       
    }
 }
