87163cda15d53f92a4c9934bf432d51a0856f244
challakeerthan <92932782+challakeerthan@users.noreply.github.com>
2022-11-28 22:11:24 -0500

    Create Adapter.java

---
+++ src/main/java/Adapter.java
@@ -0,0 +1,25 @@
+public class Adapter implements MusicPlayer {
+
+   AdvancedMediaPlayer musicplayer;
+
+   public Adapter(String audio){
+   
+      if(audio.equalsIgnoreCase("vlc") ){
+         musicplayer = new VlcPlayer();			
+         
+      }else if (audio.equalsIgnoreCase("mp4")){
+         musicplayer = new Mp4Player();
+      }	
+   }
+
+   @Override
+   public void playMusic(String audioType, String fileName) {
+   
+      if(audioType.equalsIgnoreCase("vlc")){
+         musicplayer.playVlcPlayer(fileName);
+      }
+      else if(audioType.equalsIgnoreCase("mp4")){
+         musicplayer.playMp4Player(fileName);
+      }
+   }
+}
