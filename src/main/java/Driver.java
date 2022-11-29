a0a1432b0d6ed3bd0d42925a16a2fe2789557ea1
indrajadevil001 <91915919+indrajadevil001@users.noreply.github.com>
2022-11-28 22:00:49 -0500

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,14 +1,12 @@
-public class Driver {
-    public static void main(String[] args) {
-        AudioPlayer audioPlayer = new AudioPlayer();
-        audioPlayer.playMusic("mp3", "song1.mp3");
+public class AdapterPatternDemo {
+   public static void main(String[] args) {
+      AudioPlayer in = new AudioPlayer();
+      in.playm("vlc", "inthandam.vlc");
+      in.playm("wma", "aaseethadevaina.wma");
 
-        Mp4Player mp4MusicPlayer = new Mp4Player();
-        mp4MusicPlayer.playMp4Player("song2.mp4");
-
-        VlcPlayer vlcMusicPlayer = new VlcPlayer();
-        vlcMusicPlayer.playVlcPlayer("song3.vlc");
-
-        audioPlayer.playMusic("wma", "song4.wma");
-    }
+      in.playm("mp3", "priyathama.mp3");
+      in.playm("mp4", "nannakuprematho.mp4");
+      
+      
+   }
 }
