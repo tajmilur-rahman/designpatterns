446e43f6b571b84e44ec2f9add81ec6cbf579286
indrajadevil001 <91915919+indrajadevil001@users.noreply.github.com>
2022-11-28 22:47:55 -0500

    Update Mp4Player.java

--- src/main/java/Mp4Player.java
+++ src/main/java/Mp4Player.java
@@ -1,10 +1,11 @@
+package com.company;
+
 public class Mp4Player implements AdvancedMediaPlayer {
-    @Override
+
     public void playVlcPlayer(String fileName) {
         //do nothing
     }
 
-    @Override
     public void playMp4Player(String fileName) {
         System.out.println("Playing mp4 file: " + fileName);
     }
