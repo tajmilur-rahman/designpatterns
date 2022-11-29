7bd2dce05be6f299780b1cb765efad730a14f7f9
dossanto001 <dossanto001@gannon.edu>
2022-11-28 22:02:00 -0500

    changes

--- src/main/java/MediaPlayer.java
+++ src/main/java/MediaPlayer.java
@@ -1,14 +1,13 @@
 
 public class MediaPlayer implements MusicPlayer {
+    MediaAdapter musicAdapter = new MediaAdapter("mp3");
 
-    private MediaAdapter mediaAdapter;
     @Override
     public String playMusic(String audioType, String fileName) {
         if(audioType.equalsIgnoreCase("mp3")) {
-            System.out.println("Playing mp3 file: " + fileName);
+            return "Playing mp3 file: " + fileName;
         } else {
-            System.out.println("Audio type not supported for the file: " + fileName);
+            return musicAdapter.playMusic(audioType, fileName);
         }
-        return audioType;
     }
 }
--- src/test/java/TestMediaPlayer.java
+++ src/test/java/TestMediaPlayer.java
@@ -8,11 +8,30 @@ public class TestMediaPlayer {
 
     @Test
     public void test_playMusic(){
-        MediaPlayer mp = new MediaPlayer();
-        String fileType = "mp3";
+        MediaAdapter maMp4 = new MediaAdapter("mp4");
+        String fileType = "mp4";
         String filename = "file1.mp2";
 
-        String output = mp.playMusic(fileType,filename);
-        Assert.assertEquals("Playing mp3 file "+ filename , output);
+        String output = maMp4.playMusic(fileType, filename);
+        Assert.assertEquals("Playing mp4 file: "+ filename , output);
+    }
+    @Test
+    public void test_playVlc(){
+        MediaAdapter maVlc = new MediaAdapter("vlc");
+        String fileType = "vlc";
+        String filename = "file1.mp2";
+
+        String output = maVlc.playMusic(fileType, filename);
+        Assert.assertEquals("Playing vlc file: "+ filename , output);
+    }
+
+    @Test
+    public void test_playMp4(){
+        MediaAdapter maMp4 = new MediaAdapter("mp4");
+        String fileType = "vlc";
+        String filename = "file1.mp2";
+
+        String output = maMp4.playMusic(fileType, filename);
+        Assert.assertEquals("Playing mp4 file: "+ filename , output);
     }
 }
