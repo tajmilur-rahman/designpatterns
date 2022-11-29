4a78141dcb6e705ced99cb56b68e6885f4ac1bd6
indrajadevil001 <91915919+indrajadevil001@users.noreply.github.com>
2022-11-28 22:46:44 -0500

    Update AudioPlayer.java

--- src/main/java/AudioPlayer.java
+++ src/main/java/AudioPlayer.java
@@ -1,22 +1,27 @@
+package com.company;
+
 public class AudioPlayer implements MusicPlayer {
-   MediaAdapter mediaAdapter; 
 
-   @Override
-   public void playm(String audioType, String fileName) {		
 
-      //inbuilt support to play mp3 music files
-      if(audioType.equalsIgnoreCase("mp3")){
-         System.out.println("Playing mp3 file. Name: " + fileName);			
-      } 
-      
-      //mediaAdapter is providing support to play other file formats
-      else if(audioType.equalsIgnoreCase("vlc") || audioType.equalsIgnoreCase("mp4")){
-         MusicAdapter = new MusicAdapter(audioType);
-         musicAdapter.play(audioType, fileName);
-      }
-      
-      else{
-         System.out.println("Audio type not supported for the file"+filename);
-      }
-   }   
+
+
+    public void playMusic(String audioType, String fileName) {
+        if(audioType.equalsIgnoreCase("mp3")){
+            System.out.println("Playing mp3 file. Name: " + fileName);
+        }
+        else if(audioType.equalsIgnoreCase("mp4")){
+            System.out.println("Playing mp4 file. Name: " + fileName);
+        }
+       else if(audioType.equalsIgnoreCase("vlc")){
+            System.out.println("Playing vlc file. Name: " + fileName);
+        }
+
+
+
+        else{
+            System.out.println("Audio type not supported for the file" + fileName);
+        }
+
+    }
+
 }
