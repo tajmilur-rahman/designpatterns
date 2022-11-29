50b78c28fc95dfb44cf791f2723b6fc7d877abbd
indrajadevil001 <91915919+indrajadevil001@users.noreply.github.com>
2022-11-28 22:02:30 -0500

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,4 +1,4 @@
-public class AdapterPatternDemo {
+public class Driver {
    public static void main(String[] args) {
       AudioPlayer in = new AudioPlayer();
       in.playm("vlc", "inthandam.vlc");
